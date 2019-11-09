65e8e0508cdeb650e612bcac44c1bb70beeef34c
danigcj31 <45356019+danigcj31@users.noreply.github.com>
2019-11-09 11:21:37 +0100

    M0: it1

---
+++ M0/DiagClases_M0/Mecanico.java
@@ -0,0 +1,15 @@
+package DiagClases_M0;
+
+public class Mecanico extends Trabajador {
+
+	private int prioridad = 1;
+
+	public int getPrioridad() {
+		return this.prioridad;
+	}
+
+	public void setPrioridad(int prioridad) {
+		this.prioridad = prioridad;
+	}
+
+}
