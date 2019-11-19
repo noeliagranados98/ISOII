8338cf8e065dd23a5b938a92fa2417b1c2e8ef4a
noeliagranados98 <45472537+noeliagranados98@users.noreply.github.com>
2019-11-19 17:05:56 +0100

    Generación visual M3

---
+++ M3/M3_Gestion_BBDD/Actualizar_bbdd.java
@@ -0,0 +1,20 @@
+package M3_Gestion_BBDD;
+
+public class Actualizar_bbdd {
+
+	public void insert() {
+		// TODO - implement Actualizar_bbdd.insert
+		throw new UnsupportedOperationException();
+	}
+
+	public void delete() {
+		// TODO - implement Actualizar_bbdd.delete
+		throw new UnsupportedOperationException();
+	}
+
+	public void update() {
+		// TODO - implement Actualizar_bbdd.update
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ M3/M3_Gestion_BBDD/alarma_pieza.java
@@ -0,0 +1,53 @@
+package M3_Gestion_BBDD;
+
+public class alarma_pieza {
+
+	private int id_pieza;
+	private int tipo_pieza;
+	private int umbral_calidad;
+
+	public void getId_pieza() {
+		// TODO - implement alarma_pieza.getId_pieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param id_pieza
+	 */
+	public void setId_pieza(int id_pieza) {
+		this.id_pieza = id_pieza;
+	}
+
+	public void getTipo_pieza() {
+		// TODO - implement alarma_pieza.getTipo_pieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param tipo_pieza
+	 */
+	public void setTipo_pieza(int tipo_pieza) {
+		this.tipo_pieza = tipo_pieza;
+	}
+
+	public void getUmbral_calidad() {
+		// TODO - implement alarma_pieza.getUmbral_calidad
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param umbral_calidad
+	 */
+	public void setUmbral_calidad(int umbral_calidad) {
+		this.umbral_calidad = umbral_calidad;
+	}
+
+	public void alarmar_pieza() {
+		// TODO - implement alarma_pieza.alarmar_pieza
+		throw new UnsupportedOperationException();
+	}
+
+}
