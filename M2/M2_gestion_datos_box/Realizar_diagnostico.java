cfd5343cf18db4e4233f920fd5eb9c7207ca8b74
Nerea <55734724+nereaaaa@users.noreply.github.com>
2019-11-20 13:05:10 +0100

    Fin it6

---
+++ M2/M2_gestion_datos_box/Realizar_diagnostico.java
@@ -0,0 +1,28 @@
+package M2_gestion_datos_box;
+
+public class Realizar_diagnostico {
+	 
+	private string resultado; 
+	private boolean solucion;
+	private string forma_solu;
+	
+	
+	public void getresultado() {
+
+		
+	}
+
+	public void setresultado(string resultado) {
+		this.resultado = resultado;
+	}
+	public void getsolucion() {
+		
+		
+	}
+
+	public void setsolucion(boolean solucion ) {
+		this.solucion = solucion;
+	}
+	
+	
+}
