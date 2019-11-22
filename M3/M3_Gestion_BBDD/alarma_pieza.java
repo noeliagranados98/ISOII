4c9be1a5e1359eb0104bfe9822c592d89f7f3bea
noeliagranados98 <45472537+noeliagranados98@users.noreply.github.com>
2019-11-22 13:23:58 +0100

    it9 (Módulo 3)

--- M3/M3_Gestion_BBDD/alarma_pieza.java
+++ M3/M3_Gestion_BBDD/alarma_pieza.java
@@ -6,15 +6,19 @@ public class alarma_pieza {
 	private int tipo_pieza;
 	private int umbral_calidad;
 
+
+	public alarma_pieza(int id_pieza, int tipo_pieza, int umbral_calidad) {
+		super();
+		this.id_pieza = id_pieza;
+		this.tipo_pieza=tipo_pieza;
+		this.umbral_calidad=umbral_calidad;
+	}
+
 	public void getId_pieza() {
 		// TODO - implement alarma_pieza.getId_pieza
 		throw new UnsupportedOperationException();
 	}
-
-	/**
-	 * 
-	 * @param id_pieza
-	 */
+	
 	public void setId_pieza(int id_pieza) {
 		this.id_pieza = id_pieza;
 	}
