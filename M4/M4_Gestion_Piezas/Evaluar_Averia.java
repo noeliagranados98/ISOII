123d6e0768389777796ff1e2174468f562436d52
victoravila117 <55734912+victoravila117@users.noreply.github.com>
2019-11-23 18:05:48 +0100

    clases creadas visual M4

---
+++ M4/M4_Gestion_Piezas/Evaluar_Averia.java
@@ -0,0 +1,17 @@
+package M3_Gestion_Piezas;
+
+public class Evaluar_Averia {
+
+	private int id_averia;
+
+	public void Evaluar() {
+		// TODO - implement Evaluar_Averia.Evaluar
+		throw new UnsupportedOperationException();
+	}
+
+	public void Notificar() {
+		// TODO - implement Evaluar_Averia.Notificar
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ M4/M4_Gestion_Piezas/Generar_Lista_PiezasRecambio.java
@@ -0,0 +1,10 @@
+package M3_Gestion_Piezas;
+
+public class Generar_Lista_PiezasRecambio {
+
+	public Generar_Lista_PiezasRecambio() {
+		// TODO - implement Generar_Lista_PiezasRecambio.Generar_Lista_PiezasRecambio
+		throw new UnsupportedOperationException();
+	}
+
+}
