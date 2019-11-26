26fe07b91aef7fd250bdb5c9b1cab4212c94d830
Nerea <55734724+nereaaaa@users.noreply.github.com>
2019-11-26 15:47:43 +0100

    FIN it11

--- M4/M4_Gestion_Piezas/Generar_Lista_PiezasRecambio.java
+++ M4/M4_Gestion_Piezas/Generar_Lista_PiezasRecambio.java
@@ -2,9 +2,28 @@ package M3_Gestion_Piezas;
 
 public class Generar_Lista_PiezasRecambio {
 
+	private string nombre;
+	private string pieza;
+	
+	
+  public void getnombre() {
+	  
+	}
+
+	public void setnombre(string nombre) {
+		this.nombre = nombre;
+	}
+   public void getpieza() {	
+	}
+
+	public void setpieza(string pieza) {
+		this.pieza = pieza;
+	}
+	
 	public Generar_Lista_PiezasRecambio() {
-		// TODO - implement Generar_Lista_PiezasRecambio.Generar_Lista_PiezasRecambio
+		
 		throw new UnsupportedOperationException();
+		
 	}
 
 }
