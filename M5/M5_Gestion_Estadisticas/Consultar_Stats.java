3198af1cc5ec73ab95f59968005dc058b6fb025b
Nerea <55734724+nereaaaa@users.noreply.github.com>
2019-11-27 13:29:59 +0100

    Creación M5 desde visual paradigm

---
+++ M5/M5_Gestion_Estadisticas/Consultar_Stats.java
@@ -0,0 +1,25 @@
+package M5_Gestion_Estadisticas;
+
+public class Consultar_Stats {
+
+	public void tmed_asignarbox() {
+		// TODO - implement Consultar_Stats.tmed_asignarbox
+		throw new UnsupportedOperationException();
+	}
+
+	public void tmed_rep_averias() {
+		// TODO - implement Consultar_Stats.tmed_rep_averias
+		throw new UnsupportedOperationException();
+	}
+
+	public void tmed_gen_cuenta() {
+		// TODO - implement Consultar_Stats.tmed_gen_cuenta
+		throw new UnsupportedOperationException();
+	}
+
+	public void tmed_prep_boxlibre() {
+		// TODO - implement Consultar_Stats.tmed_prep_boxlibre
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ M5/M5_Gestion_Estadisticas/Jefe_taller.java
@@ -0,0 +1,76 @@
+package M5_Gestion_Estadisticas;
+
+public class Jefe_taller {
+
+	private int id;
+	private int nombre;
+	private int apellidos;
+	private int dni;
+	private int sueldo;
+
+	public void getId() {
+		// TODO - implement Jefe_taller.getId
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void getNombre() {
+		// TODO - implement Jefe_taller.getNombre
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public void setNombre(int nombre) {
+		this.nombre = nombre;
+	}
+
+	public void getApellidos() {
+		// TODO - implement Jefe_taller.getApellidos
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param apellidos
+	 */
+	public void setApellidos(int apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public void getDni() {
+		// TODO - implement Jefe_taller.getDni
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param dni
+	 */
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public void getSueldo() {
+		// TODO - implement Jefe_taller.getSueldo
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param sueldo
+	 */
+	public void setSueldo(int sueldo) {
+		this.sueldo = sueldo;
+	}
+
+}
