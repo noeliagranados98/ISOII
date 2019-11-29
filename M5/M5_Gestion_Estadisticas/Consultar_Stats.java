aef5d89ede7656bda5a47a678ddb3dd20aed7c09
noeliagranados98 <45472537+noeliagranados98@users.noreply.github.com>
2019-11-29 09:38:50 +0100

    Fin It12

--- M5/M5_Gestion_Estadisticas/Consultar_Stats.java
+++ M5/M5_Gestion_Estadisticas/Consultar_Stats.java
@@ -2,6 +2,52 @@ package M5_Gestion_Estadisticas;
 
 public class Consultar_Stats {
 
+	String nombre;
+	String detalles;
+	String tipo;
+	int fecha;
+
+//constructor
+	public Actualizar_bbdd(String nombre, String detalles, String tipo, int fecha ) {
+		super();
+		this.nombre=nombre;
+		this.detalles=detalles;
+		this.tipo=tipo;
+		this.fecha=fecha;
+	}
+
+	public void getNombre() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void getDetalles() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setDetalles(String detalles) {
+		this.detalles = detalles;
+	}
+
+	public void getTipo() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public void getFecha() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setFecha(int fecha) {
+		this.fecha = fecha;
+	}
+
 	public void tmed_asignarbox() {
 		// TODO - implement Consultar_Stats.tmed_asignarbox
 		throw new UnsupportedOperationException();
